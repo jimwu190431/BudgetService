50036d85696dda6caa1ce266eaec1c463fdba328
jimwu <jimwu@yile.com.tw>
2022-12-11 00:13:50 +0800

    完成同年同月部分

--- src/main/java/com/yile/service/BudgetService.java
+++ src/main/java/com/yile/service/BudgetService.java
@@ -1,9 +1,65 @@
 package com.yile.service;
 
+import static java.util.stream.Collectors.toMap;
+
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.Optional;
+
+import com.yile.repo.entity.Budget;
+import com.yile.repo.inte.IBudgetRepo;
 
 public class BudgetService {
+
+	private IBudgetRepo repo;
+
+	public BudgetService(IBudgetRepo repo) {
+		this.repo = repo;
+	}
+
 	public double query(LocalDate startDate, LocalDate endDate) {
+		if (startDate.isAfter(endDate)) {
+			return 0.0;
+		}
+
+		Map<String, Budget> budgetMap = repo.getAll().stream()//
+				.collect(toMap(budget -> budget.getYearMonth(), budget -> budget));
+
+		// 同年同月
+		if ((startDate.getYear() == endDate.getYear()) //
+				&& (startDate.getMonthValue() == endDate.getMonthValue())) {
+
+			// 只需要計算當月天數
+			int diffDay = endDate.getDayOfMonth() - startDate.getDayOfMonth() + 1;
+			int monthDays = startDate.lengthOfMonth();
+			int budgetAmount = Optional.ofNullable(budgetMap.get(getBudgetKey(startDate)))//
+					.map(budget -> budget.getAmount())//
+					.orElse(0);
+
+			return (double) (diffDay * budgetAmount) / (double) monthDays;
+		}
+
+		double tempBudgetAmount = 0.0;
+		tempBudgetAmount += getStartMonthBudgetAmount(startDate);
+		tempBudgetAmount += getEndMonthBudgetAmount(endDate);
+		tempBudgetAmount += getBetweenMonthBudgetAmount(startDate, endDate);
+		return tempBudgetAmount;
+	}
+
+	private double getStartMonthBudgetAmount(LocalDate startDate) {
 		return 0.0;
+	}
+
+	private double getEndMonthBudgetAmount(LocalDate endDate) {
+		return 0.0;
+	}
+
+	private double getBetweenMonthBudgetAmount(LocalDate startDate, LocalDate endDate) {
+		return 0.0;
+	}
+
+	private String getBudgetKey(LocalDate localDate) {
+		return localDate.format(DateTimeFormatter.ofPattern("yyyyMM"));
 	}
 }
--- src/test/java/com/yile/service/BudgetServiceTest.java
+++ src/test/java/com/yile/service/BudgetServiceTest.java
@@ -1,14 +1,59 @@
 package com.yile.service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.yile.repo.entity.Budget;
+import com.yile.repo.inte.IBudgetRepo;
 
 public class BudgetServiceTest {
 
+	private IBudgetRepo repo;
+	private BudgetService service;
+
+	@Before
+	public void before() {
+		repo = Mockito.mock(IBudgetRepo.class);
+		service = new BudgetService(repo);
+
+		List<Budget> budgets = new ArrayList<>();
+		budgets.add(createBudget("202111", 30 * 1));
+		budgets.add(createBudget("202112", 31 * 10));
+		budgets.add(createBudget("202201", 31 * 100));
+		budgets.add(createBudget("202202", 28 * 1000));
+		budgets.add(createBudget("202203", 31 * 10000));
+		Mockito.when(repo.getAll()).thenReturn(budgets);
+	}
+
+	private Budget createBudget(String yearMonth, int amount) {
+		Budget budget = new Budget();
+		budget.setYearMonth(yearMonth);
+		budget.setAmount(amount);
+		return budget;
+	}
+
+
 	@Test
-	public void test1() {
-		BudgetService service = new BudgetService();
-		double result = service.query(null, null);
-		Assert.assertEquals(result, 0.0, 0.00);
+	public void test_same_month_same_day() {
+		double result = service.query(LocalDate.of(2022, 01, 01), LocalDate.of(2022, 01, 01));
+		Assert.assertEquals(1 * 100, result, 0.00);
+	}
+	
+	@Test
+	public void test_same_month_diff_day() {
+		double result = service.query(LocalDate.of(2022, 02, 01), LocalDate.of(2022, 02, 02));
+		Assert.assertEquals(2 * 1000, result, 0.00);
+	}
+	
+	@Test
+	public void test_all_month() {
+		double result = service.query(LocalDate.of(2022, 03, 01), LocalDate.of(2022, 03, 31));
+		Assert.assertEquals(31 * 10000, result, 0.00);
 	}
 }
